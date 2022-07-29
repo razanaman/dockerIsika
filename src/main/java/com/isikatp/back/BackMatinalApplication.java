96a2b423faececd5eb60868854c5432b745bee2c
Alain Nimbol <alainnimbol@gmail.com>
2022-07-29 13:00:02 +0300

    1st Commit

---
+++ src/main/java/com/isikatp/back/BackMatinalApplication.java
@@ -0,0 +1,13 @@
+package com.isikatp.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMatinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackMatinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isikatp/back/BackMatinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isikatp.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackMatinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
