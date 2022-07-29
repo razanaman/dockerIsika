819dc7b25bd8798af4de2d436dccde5d7525a300
Alain Nimbol <alainnimbol@gmail.com>
2022-07-29 15:20:35 +0300

    3rd Commit

---
+++ src/main/java/com/isikatp/back/model/Personne.java
@@ -0,0 +1,13 @@
+package com.isikatp.back;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Personne {
+    private String nom;
+
+}
