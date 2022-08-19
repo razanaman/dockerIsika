d6073ed90a4b89f700216208f7479a1701b09ff0
Alain Nimbol <alainnimbol@gmail.com>
2022-08-19 17:28:43 +0300

    last2 Commit test

--- src/main/java/com/isikatp/back/controller/BonjourController.java
+++ src/main/java/com/isikatp/back/controller/BonjourController.java
@@ -1,11 +1,8 @@
 package com.isikatp.back.controller;
 
-import com.isikatp.back.model.Personne;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 
 @Controller
 @RequestMapping("/")
